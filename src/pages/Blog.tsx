d80bf437d34a055c1448755970ddaa306c2a788e
judiciousmurich <murichjudicious45@gmail.com>
2023-10-06 16:15:47 +0300

    feat:add styles

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -8,13 +8,12 @@ const Blog: React.FC = () => {
     <>
       {/* <BackHome /> */}
       <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 p-6">
-        <News
           image="https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/10/adult-blur-close-up-267394-1024x573.jpg"
           date="October 5, 2019"
           author="Paul"
           title="HOW to choose perfect gadgets"
           description="When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper s ..."
-        />
+        {/* /> */}
         {/* Other News components */}
       </div>
       {/* <Clients /> */}
