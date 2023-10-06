d256b7db1aa652ff9d6441a1f9855ea970585335
judiciousmurich <murichjudicious45@gmail.com>
2023-10-06 15:46:31 +0300

    feat:add styles

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import News from '../../shared/News';
+import Clients from '../../shared/Clients';
+import BackHome from '../../shared/BackHome';
+
+const Blog: React.FC = () => {
+  return (
+    <>
+      <BackHome />
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 p-6">
+        <News
+          image="https://demo.phlox.pro/shop-digital/wp-content/uploads/sites/127/2019/10/adult-blur-close-up-267394-1024x573.jpg"
+          date="October 5, 2019"
+          author="Paul"
+          title="HOW to choose perfect gadgets"
+          description="When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper s ..."
+        />
+        {/* Other News components */}
+      </div>
+      <Clients />
+    </>
+  );
+};
+
+export default Blog;
